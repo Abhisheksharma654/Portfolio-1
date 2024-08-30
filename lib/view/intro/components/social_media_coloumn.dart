fdffa49c502b8833eeb4c1fc9b7443e8f75ac5ef
Abhisheksharma654 <Abhisheksharma654@gmail.com>
2024-08-30 16:27:31 +0530

    first commit

--- lib/view/intro/components/social_media_coloumn.dart
+++ lib/view/intro/components/social_media_coloumn.dart
@@ -9,11 +9,22 @@ class SocialMediaIconColumn extends StatelessWidget {
   Widget build(BuildContext context) {
     return Column(
       children: [
-        SocialMediaIcon(icon: 'assets/icons/linkedin.svg',onTap: ()=>launchUrl(Uri.parse('https://www.linkedin.com/in/hamad-anwar/'))),
-        SocialMediaIcon(icon: 'assets/icons/github.svg',onTap: () => launchUrl(Uri.parse('https://github.com/Hamad-Anwar')),),
-        const SocialMediaIcon(icon: 'assets/icons/dribble.svg',),
-        const SocialMediaIcon(icon: 'assets/icons/twitter.svg'),
-        const SocialMediaIcon(icon: 'assets/icons/linkedin.svg'),
+        SocialMediaIcon(
+            icon: 'assets/icons/linkedin.svg',
+            onTap: () => launchUrl(Uri.parse(
+                'https://www.linkedin.com/in/abhishek-sharma-123844100/'))),
+        SocialMediaIcon(
+          icon: 'assets/icons/github.svg',
+          onTap: () =>
+              launchUrl(Uri.parse('https://github.com/Abhisheksharma654')),
+        ),
+        const SocialMediaIcon(
+          icon: 'assets/icons/dribble.svg',
+        ),
+        SocialMediaIcon(
+          icon: 'assets/icons/twitter.svg',
+          onTap: () => launchUrl(Uri.parse('https://x.com/abhishek9871')),
+        ),
       ],
     );
   }
--- lib/view/main/components/drawer/personal_info.dart
+++ lib/view/main/components/drawer/personal_info.dart
@@ -11,15 +11,20 @@ class PersonalInfo extends StatelessWidget {
     return const Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        SizedBox(height: defaultPadding/2,),
+        SizedBox(
+          height: defaultPadding / 2,
+        ),
         AreaInfoText(title: 'Contact', text: '03166657602'),
-        AreaInfoText(title: 'Email', text: 'rh676838@gmail.com'),
-        AreaInfoText(title: 'LinkedIn', text: '@hamad-anwar'),
-        AreaInfoText(title: 'Github', text: '@hamad-anwar'),
+        AreaInfoText(title: 'Email', text: 'abhisheksharma6548@gmail.com'),
+        AreaInfoText(title: 'LinkedIn', text: '@abhishek-sharma-123844100'),
+        AreaInfoText(title: 'Github', text: '@Abhisheksharma654'),
         SizedBox(
           height: defaultPadding,
         ),
-        Text('Skills',style: TextStyle(color: Colors.white),),
+        Text(
+          'Skills',
+          style: TextStyle(color: Colors.white),
+        ),
         SizedBox(
           height: defaultPadding,
         ),
